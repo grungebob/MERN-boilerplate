f4fe3931b507c57c6f2c344fd6d4b3d1f1392591
grungebob <chungrob@gmail.com>
2018-07-30 11:26:07 -0500

    Under Construction Header

--- client/app/components/Header/Header.js
+++ client/app/components/Header/Header.js
@@ -4,6 +4,11 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
   <header>
+  
+    <p>robchung.com is under construction right now. <br />
+       Updates coming soon. <br />
+       Email me at chungrob@gmail.com in the meantime if you have questions. <br />
+    </p>
     <Link to="/">Home</Link>
 
     <nav>
